c186b0d8e568681b8cb82dc1ba5cb352e60ecb0d
Brian Meeker <meeker.brian@gmail.com>
2022-10-07 11:31:41 -0400

    Add the astro-robots-txt integration to generate the robots.txt file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from 'astro-robots-txt';
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
@@ -6,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brianmeeker.me/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
